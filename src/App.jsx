a5ef6eaa11fed9cae6269e58f6a3b54f7c725ed4
DelighteningPigeon <89842170+fillpattana@users.noreply.github.com>
2025-05-17 12:42:12 +0700

    tailwind and default fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-import { useState } from "react";
 import Home from "./pages/Home.jsx";
 import PromptPayQR from "./components/PromptPayQR.jsx";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./styles/App.css";
-// import { defineConfig } from "vite";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      {/* <Home /> */}
-      <PromptPayQR />
+      <Home />
+      {/* <PromptPayQR /> */}
       //
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,9 @@ export default function Home() {
   return (
     <>
       <div>
-        <h1>Hello</h1>
+        <h1 className="text-red-800">
+          สวัสดีนี่คือ Jewel Matters ขอบคุณที่ใช้บริการ
+        </h1>
       </div>
     </>
   );
