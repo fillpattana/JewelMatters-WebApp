0387e235881c9e9e2d59db5814a9614971354131
DelighteningPigeon <89842170+fillpattana@users.noreply.github.com>
2025-05-18 11:25:11 +0700

    toggled Line Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Home from "./employee-pages/CreateSlip.jsx";
 import CreateSlip from "./components/PromptPayQR.jsx";
+import LineLogin from "./components/LineLogin.jsx";
 import "./styles/App.css";
 
 function App() {
   return (
     <>
-      <CreateSlip />
+      <LineLogin />
     </>
   );
 }
