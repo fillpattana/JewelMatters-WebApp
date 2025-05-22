d6633cc62c9d82afe57d24a76c799396e62a98d0
DelighteningPigeon <89842170+fillpattana@users.noreply.github.com>
2025-05-22 19:54:05 +0700

    deployed test

--- src/employee-pages/Home.jsx
+++ src/employee-pages/Home.jsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
       <div>
-        <h1 className="text-red-800">
+        <h1 className="text-green-600">
           สวัสดีนี่คือ Jewel Matters ขอบคุณที่ใช้บริการ
         </h1>
       </div>
