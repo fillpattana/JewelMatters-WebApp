463e3c132a76ad52386b09963905bfcbfaefc77d
DelighteningPigeon <89842170+fillpattana@users.noreply.github.com>
2025-05-24 12:34:14 +0700

    Start Sale Period Setup
    Start Sale Period Setup for owner

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import LineLogin from "./components/LineLogin.jsx";
 import CreateSlip from "./employee-pages/CreateSlip.jsx";
 import Home from "../src/employee-pages/Home.jsx";
 import CreateNewProduct from "./storeowner-pages/CreateNewProduct.jsx";
+import StartSalePeriod from "./storeowner-pages/StartSalePeriod.jsx";
 import "./styles/App.css";
 
 function PageRouter() {
@@ -13,6 +14,7 @@ function PageRouter() {
     createslip: <CreateSlip />,
     linelogin: <LineLogin />,
     createnewproduct: <CreateNewProduct />,
+    startsaleperiod: <StartSalePeriod />,
   };
 
   return pages[page] || <Home />; // fallback to <Home /> if page is not recognized
