005151ca9738ffaeca05c3c1dadbeaef6f0c2e4a
DelighteningPigeon <89842170+fillpattana@users.noreply.github.com>
2025-06-13 11:13:11 +0700

    sanity check

--- src/components/CalendarTest.jsx
+++ src/components/CalendarTest.jsx
@@ -1,43 +1,55 @@
+// import React, { useState } from "react";
+// import {
+//   Popover,
+//   PopoverTrigger,
+//   PopoverContent,
+// } from "@/components/ui/popover";
+// import { Calendar } from "@/components/ui/calendar";
+// import { Button } from "@/components/ui/button";
+// import { format } from "date-fns";
+// import { th } from "date-fns/locale";
+
+// export default function CalendarTest() {
+// const [date, setDate] = useState(new Date());
+// const [open, setOpen] = useState(false);
+// const handleSelect = (selectedDate) => {
+//   if (selectedDate) {
+//     setDate(selectedDate);
+//     setOpen(false); // Close popover only after a date is selected
+//   }
+// };
+// return (
+//   <Popover open={open} onOpenChange={setOpen}>
+//     <PopoverTrigger asChild>
+//       <Button variant="outline" onClick={() => setOpen(!open)}>
+//         {format(date, "PPP", { locale: th })}
+//       </Button>
+//     </PopoverTrigger>
+//     <PopoverContent className="w-auto p-0 z-50 bg-white">
+//       <Calendar
+//         mode="single"
+//         selected={date}
+//         onSelect={handleSelect}
+//         initialFocus
+//       />
+//     </PopoverContent>
+//   </Popover>
+// );
+// }
+
 import React, { useState } from "react";
-import {
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-} from "@/components/ui/popover";
 import { Calendar } from "@/components/ui/calendar";
-import { Button } from "@/components/ui/button";
-import { format } from "date-fns";
-import { th } from "date-fns/locale";
 
 export default function CalendarTest() {
-  // const [date, setDate] = useState(new Date());
-  // const [open, setOpen] = useState(false);
-  // const handleSelect = (selectedDate) => {
-  //   if (selectedDate) {
-  //     setDate(selectedDate);
-  //     setOpen(false); // Close popover only after a date is selected
-  //   }
-  // };
-  // return (
-  //   <Popover open={open} onOpenChange={setOpen}>
-  //     <PopoverTrigger asChild>
-  //       <Button variant="outline" onClick={() => setOpen(!open)}>
-  //         {format(date, "PPP", { locale: th })}
-  //       </Button>
-  //     </PopoverTrigger>
-  //     <PopoverContent className="w-auto p-0 z-50 bg-white">
-  //       <Calendar
-  //         mode="single"
-  //         selected={date}
-  //         onSelect={handleSelect}
-  //         initialFocus
-  //       />
-  //     </PopoverContent>
-  //   </Popover>
-  // );
+  const [date, setDate] = useState(new Date());
+
   return (
-    <PopoverTrigger asChild>
-      <Button>Click</Button>
-    </PopoverTrigger>
+    <Calendar
+      mode="single"
+      selected={date}
+      onSelect={setDate}
+      className="rounded-md border shadow-sm"
+      captionLayout="dropdown"
+    />
   );
 }
